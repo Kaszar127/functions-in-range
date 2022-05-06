c2d1e29907babc5ab4bb432df8349bec7c18e4de
SilasCh <88002368+Kaszarr@users.noreply.github.com>
2022-05-06 14:07:17 +0200

    Created base structure and prepared the formulas

--- main.py
+++ main.py
@@ -1,1 +1,71 @@
+import math
+import random
+from tkinter import Y
+
+# Range area
+range = (0,100)
+
+def linear():
+    # Linear variables
+    y = random.randInt(range[0], range[1])
+    a = (y2-y1)/(x2-x1)
+    x = range[1]
+    b = y-(a*x)
+
+    formula = a*x+b
+
+def secondDegreePolynomial():
+    # Second degree polynomial variables
+    y = random.randInt(range[0], range[1])
+    a = 123
+    x = 123
+    b = 123
+    c = 123
+
+    formula = a*(x**2)+(b*x)+c
+
+def exponential():
+    # Exponential variables
+    y = random.randInt(range[0], range[1])
+    a = 123
+    x = 123
+    b = 123
+
+    formula = b*(a**x)
+
+def sinus():
+    # Sinus variables
+    y = random.randInt(range[0], range[1])
+    a = 123
+    x = 123
+    b = 123
+    c = 123
+    
+    formula = a*(math.sin(b*x+c))
+
+# Stores all relevant functions
+functionList = [linear(), secondDegreePolynomial(), exponential(), sinus()]
+
+# Point 1 coords
+x1 = range[0]
+y1 = random.randInt(range[0],range[1])
+
+# Point 2 coords
+x2 = range[1]
+y2 = random.choice(functionList)
+
+# Points
+p1 = (x1, y2)
+p2 = (x2, y2)
+
+# Store results
+resultList = []
+
+
+
+for i in range(100):
+    if(123 == 1):
+        123
+
+
 
